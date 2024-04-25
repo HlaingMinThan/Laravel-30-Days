38d8b05c3408f1ad311fcad8bf70ec0fad36961d
HlaingMinThan <hlaingminthan@92@gmail.com>
2024-04-25 14:44:48 +0700

    day2

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/about', function () {
+    // return 'about page';
+    // return [
+    //     'foo' => 'bar'
+    // ];
+    return view('about');
+});
+
+//homework for contact route
