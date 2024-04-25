76874b970aa138474a2a1d0a9ddd70d3fb3bfa79
HlaingMinThan <hlaingminthan@92@gmail.com>
2024-04-25 17:14:46 +0700

    day-5 nav-link real world component

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -30,19 +30,18 @@
                         </div>
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
-                                <a
+                                <x-nav-link
                                     href="/"
-                                    class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
-                                    aria-current="page"
-                                >Home</a>
-                                <a
+                                    :active="request()->is('/')"
+                                >Home</x-nav-link>
+                                <x-nav-link
                                     href="/about"
-                                    class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-                                >About</a>
-                                <a
+                                    :active="request()->is('about')"
+                                >About</x-nav-link>
+                                <x-nav-link
                                     href="/contact"
-                                    class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-                                >Contact</a>
+                                    :active="request()->is('contact')"
+                                >Contact</x-nav-link>
                             </div>
                         </div>
                     </div>
@@ -147,7 +146,7 @@
                     <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                     <a
                         href="/"
-                        class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
+                        class="{{false ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} block rounded-md px-3 py-2 text-base font-medium"
                         aria-current="page"
                     >Home</a>
                     <a
--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,1 +1,6 @@
-<a {{$attributes}}>{{$slot}}</a>
+@props(['active'])
+<a
+    {{$attributes}}
+    class="{{$active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
+    aria-current="page"
+>{{$slot}}</a>
